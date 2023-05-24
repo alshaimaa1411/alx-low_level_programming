c015c314358b0b9a78442b6d3bf8b0316b03cdba
alshaimaa1411 <ahmedameen1411@gmail.com>
2023-05-24 09:20:43 -0700

    0t

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+int main(void)
+{#include <stdio.h>
+	/* more headers goes there */
+
+	/* betty style doc for function main goes there */
+	int main(void)
+	{_putchar();
+		return (0);
+	}
