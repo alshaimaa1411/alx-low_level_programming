38810dccdfe7f4d9a3aa6d7d92eb2a5dfb29bc4f
alshaimaa1411 <ahmedameen1411@gmail.com>
2023-05-29 14:12:56 -0700

    5t

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -14,17 +14,17 @@ int print_sign(int n)
 {
 	if (n > 0)
 	{
-		printf("+\n");
+		printf("+1\n");
 	return (1);
 	}
 	else if (n == 0)
 	{
-		printf("0\n");
+		printf("00\n");
 return (0);
 	}
 	else
 	{
-		printf("-\n");
+		printf("-1\n");
 return (-1);
 	}
 }
