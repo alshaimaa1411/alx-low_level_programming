e9fd1a391b8dd43c869487b902c11c9d9a9beb0c
alshaimaa1411 <ahmedameen1411@gmail.com>
2023-06-06 16:36:28 -0700

    4t

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_most_numbers - print nums exept 2 and 4.
+ */
+
+void print_most_numbers(void)
+{
+	int x;
+
+for (x = 48; x < 58; x++)
+if (x == 50 || x == 52)
+continue;
+_putchar(x);
+_putchar('\n');
+}
