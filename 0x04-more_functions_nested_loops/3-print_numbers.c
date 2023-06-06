0c5118884150f7e5ac03c2c3c9bf3aaa7a865eb2
alshaimaa1411 <ahmedameen1411@gmail.com>
2023-06-06 15:10:57 -0700

    3t

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include<stdio.h>
+/**
+ * print_numbers - print numbers from 0 to 9.
+ *
+ */
+
+void print_numbers(void);
+{
+	int x;
+
+	for (x = 0; x < 10; x++)
+		_purchar(x)
+			_putchar('\n')
+}
