9e3ce92fe51c06ff6e2ab64ed26860bb912fd9a2
alshaimaa1411 <ahmedameen1411@gmail.com>
2023-06-06 17:57:33 -0700

    7t

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include<stdio.h>
+/**
+ * print_diagonal - num of / printed.
+ * @n: tolen int.
+ */
+void print_diagonal(int n)
+{
+	while (n-- > 0)
+		_putchar(92);
+			_putchar('\n');
+}
