10c81e14921534e078b35e98fa0adee97c2af166
alshaimaa1411 <ahmedameen1411@gmail.com>
2023-06-06 17:44:28 -0700

    6t

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include<stdio.h>
+/**
+ * print_line - print _ equal n.
+ * @n: input taken.
+ */
+void print_line(int n)
+{
+	int x;
+
+	for (x = 0; x <= n; x++)
+{
+	_putchar(95);
+}
+_putchar('\n');
+}
