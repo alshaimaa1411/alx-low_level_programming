df10f1a3248c6ad10f348c1831ff6d056e2f8bfa
alshaimaa1411 <ahmedameen1411@gmail.com>
2023-06-12 18:01:15 -0700

    t

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -14,8 +14,16 @@ int main(int argc, char *argv[])
 	int x;
 	int y;
 
+	if (argc == 3)
+	{
 	x = _atoi(argv[1]);
 	y = _atoi(argv[2]);
 	printf("%d\n", x * y);
-	return (1);
+	return (0);
+}
+else
+{
+	printf("Error\n");
+return (1);
+}
 }
