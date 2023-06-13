4a77106f899fd55172b9980cdd4ab7d5a32c9cd0
alshaimaa1411 <ahmedameen1411@gmail.com>
2023-06-12 17:28:27 -0700

    t

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include<stdio.h>
+/**
+ * main - prints all arguments it receives.
+ * @argc: num of arg.
+ * @argv: array of arg.
+ *
+ * Return: 0.
+ */
+int main(int argc, char *argv[])
+{
+while (argc = 1)
+do {
+argc++;
+printf("%d\n", argc);
+return (0);
+}
+}
