3e160203fa78058e1cb095f21bcf8b40b74a7c2f
alshaimaa1411 <ahmedameen1411@gmail.com>
2023-06-19 16:00:50 -0700

    t

--- 0x0D-preprocessor/0-object_like_macro.h
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -1,10 +1,6 @@
-#ifndef MACRO_H 
- #define MACRO_H 
- /** 
-  * File: O-object_like_macro 
-  * 
-  * Desc: haeder file 
-  */ 
- #define SIZE 1024 
-  
- #endif
+#ifdef _OBJECT_LIKE_MACRO_H
+#define _OBJECT_LIKE_MACRO_H
+
+#define SIZE 1024
+
+#endif /*_OBJECT_LIKE_MACRO_H */
