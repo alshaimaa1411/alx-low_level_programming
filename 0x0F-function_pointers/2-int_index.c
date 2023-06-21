e4fcd7efa97ac3624284ff97abea7b129ac56c35
alshaimaa1411 <ahmedameen1411@gmail.com>
2023-06-21 16:31:50 -0700

    t

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -3,7 +3,7 @@
  * int_index - searches for an integer.
  * @size: number of elements in the array.
  * @cmp:  pointer to the function to be used to compare values.
- *
+ * @array: arr pf para
  * Return: eturns the index of the first element
  * for which the cmp function does not return 0
  */
