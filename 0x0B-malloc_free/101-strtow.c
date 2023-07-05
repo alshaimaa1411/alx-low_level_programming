4746383d8fd1824e671bc89101f17f963cc54dcb
alshaimaa <ahmedameen1411@gmail.com>
2023-07-05 13:29:18 -0700

    t

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -7,39 +7,39 @@
  */
 char **strtow(char *str)
 {
-	char **ptr;
-	int i, k, len, start, end, j = 0;
-	int words =  countWords(str);
+char **ptr;
+int i, k, len, start, end, j = 0;
+int words =  countWords(str);
 
-	if (!str || !countWords(str))
-		return (NULL);
-	ptr = malloc(sizeof(char *) * (words + 1));
-	if (!ptr)
-		return (NULL);
-	for (i = 0; i < words; i++)
-	{
-		start = startIndex(str, j);
-		end = endIndex(str, start);
-		len = end - start;
-		ptr[i] = malloc(sizeof(char) * (len + 1));
-		if (!ptr[i])
-		{
-			i -= 1;
-			while (i >= 0)
-			{
-				free(ptr[i]);
-					i--;
-			}
-			free(ptr);
-			return (NULL);
-		}
-		for (k = 0; k < len; k++)
-			ptr[i][k] = str[start++];
-		ptr[i][k++] = '\0';
-		j = end + 1;
-	}
-	ptr[i] = NULL;
-	return (ptr);
+if (!str || !countWords(str))
+return (NULL);
+ptr = malloc(sizeof(char *) * (words + 1));
+if (!ptr)
+return (NULL);
+for (i = 0; i < words; i++)
+{
+start = startIndex(str, j);
+end = endIndex(str, start);
+len = end - start;
+ptr[i] = malloc(sizeof(char) * (len + 1));
+if (!ptr[i])
+{
+i -= 1;
+while (i >= 0)
+{
+free(ptr[i]);
+i--;
+}
+free(ptr);
+return (NULL);
+}
+for (k = 0; k < len; k++)
+ptr[i][k] = str[start++];
+ptr[i][k++] = '\0';
+j = end + 1;
+}
+ptr[i] = NULL;
+return (ptr);
 }
 
 /**
@@ -49,7 +49,7 @@ char **strtow(char *str)
  */
 int isSpace(char c)
 {
-	return (c == ' ');
+return (c == ' ');
 }
 
 /**
@@ -61,9 +61,9 @@ int isSpace(char c)
 int startIndex(char *s, int index)
 {
 
-	while (isSpace(*(s + index)))
-		index++;
-	return (index);
+while (isSpace(*(s + index)))
+index++;
+return (index);
 }
 
 /**
@@ -74,9 +74,9 @@ int startIndex(char *s, int index)
  */
 int endIndex(char *s, int index)
 {
-	while (!isSpace(*(s + index)))
-		index++;
-	return (index);
+while (!isSpace(*(s + index)))
+index++;
+return (index);
 }
 
 /**
@@ -86,19 +86,19 @@ int endIndex(char *s, int index)
  */
 int countWords(char *s)
 {
-	int wordOn = 0;
-	int words = 0;
+int wordOn = 0;
+int words = 0;
 
-	while (*s)
-	{
-		if (isSpace(*s) && wordOn)
-			wordOn = 0;
-		else if (!isSpace(*s) && !wordOn)
-		{
-			wordOn = 1;
-			words++;
-		}
-		s++;
-	}
-	return (words);
+while (*s)
+{
+if (isSpace(*s) && wordOn)
+wordOn = 0;
+else if (!isSpace(*s) && !wordOn)
+{
+wordOn = 1;
+words++;
+}
+s++;
+}
+return (words);
 }
