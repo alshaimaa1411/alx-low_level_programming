6439858bd55dc13541f18342a6dd63d3ed588f68
alshaimaa1411 <ahmedameen1411@gmail.com>
2024-02-21 13:47:23 -0800

    t

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -4,17 +4,20 @@
  * @str: char to check
  * Return: 0 is success
  */
-void puts_half(char *str){
+void puts_half(char *str)
+{
 int i = 0;
 int x = strlen(str);
 int n;
-if (x %2 != 0){
+if (x % 2 != 0)
+{
 n = (x - 1) / 2;
 }
-else {
+else
+{
 n = x / 2;
 }
-for (i = n; i < = x; i++)
+for (i = n; i <= x; i++)
 {
 char temp = str[i];
 putchar(str[i]);
