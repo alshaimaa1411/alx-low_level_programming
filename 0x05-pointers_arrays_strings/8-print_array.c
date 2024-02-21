2824af2dfad682eb7b68592d4392ee087a4c549d
alshaimaa1411 <ahmedameen1411@gmail.com>
2024-02-21 14:17:38 -0800

    t

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_array - prints half of a string
+ * @a: char to check
+ * @n: num
+ * Return: 0 is success
+ */
+void print_array(int *a, int n)
+{
+int i;
+for (i = 0; i < n; i++)
+{
+printf("%d", a[i]);
+if (i == n - 1)
+{
+}
+else
+{
+putchar(',');
+}
+putchar(' ');
+}
+printf("\n");
+}
